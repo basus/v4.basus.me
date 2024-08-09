ce9d6f97af694cd7d9e94e8596fc87630b2c0ef5
Shrutarshi Basu <shr@basus.me>
2024-08-09 12:56:05 +0530

    Add pagination for individual stream post types

--- src/stream/pages.page.js
+++ src/stream/pages.page.js
@@ -1,6 +1,43 @@
 export const layout = "layouts/stream/page.vto";
 
-export default function* ({ search, paginate }) {
+// TODO: find a way to read this out the _data.yml
+const postTypes = [ "fleet", "link", "quote", "images", "video" ];
+
+function* makeTypePages({ search, paginate }) {
+
+  for (const postType of postTypes) {
+
+    const condition = `section=stream basename!=index post_type=${postType}`;
+    const posts = search.pages(condition, "date=desc");
+    const options = {
+      url: (n) => `/stream/${postType}/${n}/`,
+      size: 10,
+    };
+
+    for (const page of paginate(posts, options)) {
+      yield page;
+    }
+
+    // Finally build the index page
+    const indexPage = search.page(`section=stream basename=index post_type=${postType}`);
+
+    if (!indexPage) {
+      const condition = `section=stream basename!=index post_type=${postType}`;
+      yield {
+        url: `/stream/${postType}/index.html`,
+        content: "hello, world!",
+        results: search.pages(condition, "date=desc", 10),
+        layout: layout,
+        pagination : {
+          previous : "",
+          next: "/stream/${postType}/2",
+        }
+      }
+    }
+  }
+}
+
+function* makeStreamPages({ search, paginate }) {
   const posts = search.pages("section=stream basename!=index", "date=desc");
   const options = {
     url: (n) => `/stream/page/${n}/`,
@@ -10,4 +47,10 @@ export default function* ({ search, paginate }) {
   for (const page of paginate(posts, options)) {
     yield page;
   }
+}
+
+
+export default function* ({ search, paginate }) {
+  yield* makeStreamPages({ search, paginate });
+  yield* makeTypePages({ search, paginate });
 }
