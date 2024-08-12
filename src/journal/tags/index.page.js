ebb0d77fb237a21c65ce948e3821c77efe94905b
Shrutarshi Basu <shr@basus.me>
2024-08-12 21:05:13 +0530

    Fix styling for tags index pages

--- src/journal/tags/index.page.js
+++ src/journal/tags/index.page.js
@@ -5,6 +5,7 @@ export default function* ({ search }) {
   for (const tag of search.values("tags")) {
     const entries = search.pages(`"${tag}"`, "date=desc");
     yield {
+      subsection: "tags",
       url: `/journal/tags/${tag}/`,
       layout : "layouts/tag-page.vto",
       title: tag,
